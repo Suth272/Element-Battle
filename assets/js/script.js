846d3b154930ce85ac55bdc7f940cb334b444d7d
Suth272 <suth272@outlook.com>
2024-05-25 23:48:46 +0100

    FEAT: Added decision making on whether the player or the computer won.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,51 @@
+//Establishing variables
 let choices = ['fire', 'air', 'earth', 'water'];
 let playerChose = document.getElementById('playerChose');
 let computerChose = document.getElementById('computerChose');
 let resultDisplay = document.getElementById('resultDisplay');
 
 function play(playerChoice) {
-
+    //Generates computer's choice
     let computerChoice = choices[Math.floor(Math.random() * 4)]
-    console.log(computerChoice)
-
+    let result = '';
+    //Deciding who wins
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE!";
+    } else {
+        switch (playerChoice) {
+            case 'fire':
+                if (computerChoice === 'air') {
+                    result = 'YOU WIN!';
+                } else if (computerChoice === 'earth') {
+                    result = "IT'S A TIE!";
+                } else {
+                    result = 'YOU LOSE!';
+                }
+            case 'air':
+                if (computerChoice === 'earth') {
+                    result = 'YOU WIN!';
+                } else if (computerChoice === 'water') {
+                    result = "IT'S A TIE!";
+                } else {
+                    result = 'YOU LOSE!';
+                }
+            case 'earth':
+                if (computerChoice === 'water') {
+                    result = 'YOU WIN!';
+                } else if (computerChoice === 'fire') {
+                    result = "IT'S A TIE!";
+                } else {
+                    result = 'YOU LOSE!';
+                }
+            case 'water':
+                if (computerChoice === 'fire') {
+                    result = 'YOU WIN!';
+                } else if (computerChoice === 'air') {
+                    result = "IT'S A TIE!";
+                } else {
+                    result = 'YOU LOSE!';
+                }
+        }
+    }
+    
 }
